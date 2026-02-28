c3fd76f22ea693b16997517b5dc6ab9a8eb5b2ab
Kamille <25205945+kamiTT@users.noreply.github.com>
2026-02-28 18:43:56 -0500

    add socket middleware to server

--- apps/backend/app.js
+++ apps/backend/app.js
@@ -5,6 +5,10 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  req.io = app.get('io');
+  next();
+});
 
 app.get('/', (req, res) => {
   res.json({ ok: true });
@@ -16,5 +20,27 @@ app.get('/health', (req, res) => {
 
 app.use('/tasks', tasksRouter);
 app.use('/users', usersRouter);
+
+app.applySocketMiddleware = (io) => {
+  app.set('io', io);
+
+  io.use((socket, next) => {
+    const requiredToken = process.env.SOCKET_IO_TOKEN;
+    const providedToken = socket.handshake.auth && socket.handshake.auth.token;
+
+    if (requiredToken && providedToken !== requiredToken) {
+      return next(new Error('Unauthorized'));
+    }
+
+    next();
+  });
+
+  io.on('connection', (socket) => {
+    console.log('Socket connected:', socket.id);
+    socket.on('disconnect', () => {
+      console.log('Socket disconnected:', socket.id);
+    });
+  });
+};
 
 module.exports = app;
